bd2d23e715bd05fdd6d3dcda7fc6aef0fbc547d6
housenkui <housenkui@gmail.com>
2020-04-25 18:14:52 +0800

    添加ffmpeg、SDL库

---
+++ Player/Player/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  Player
+//
+//  Created by 侯森魁 on 2020/4/25.
+//  Copyright © 2020 侯森魁. All rights reserved.
+//
+
+#include <stdio.h>
+#include "SDL.h"
+
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libswscale/swscale.h"
+#include "libswresample/swresample.h"
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
